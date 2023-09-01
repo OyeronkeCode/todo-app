dd8a93dbae916e9c27e4c84ad0552b15c03fc729
OyeronkeCode <oyeronkediana@gmail.com>
2023-09-01 08:19:07 +0100

    react-router-dom install

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.scss';
+// react-router-dom is an applicationa that allows you to create navigation in your app.
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-
   return (
-   <>
-   Hello World
-   </>
-  )
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home/>} />
+   </Routes>
+   </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
