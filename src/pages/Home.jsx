de3f31b6036839c961bf5b58563cc97289bb986d
OyeronkeCode <oyeronkediana@gmail.com>
2023-09-14 23:00:09 +0100

    adding components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Container from '../components/container/Container'
+import Header from'../components/header/Header'
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Container>
+      <Header/>
+    </Container>
+ )
 }
